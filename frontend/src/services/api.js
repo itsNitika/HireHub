244c12cf2bb8c2377ea6a990c30f29cad45fadd2
itsNitika <231230039@nitdelhi.ac.in>
2026-09-16 17:37:01 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://placepro-omn2.onrender.com/api",
+  baseURL: "https://hirehub-i27u.onrender.com",
 });
 
 export default API;
